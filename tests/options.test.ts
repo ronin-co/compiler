ee83d87f5e22374f387cbe5ebd321b47e6dc24f2
Leo Lamprecht <leo@ronin.co>
2024-11-08 13:31:48 +0100

    Added test for inlining statement values

---
+++ tests/options.test.ts
@@ -0,0 +1,34 @@
+import { expect, test } from 'bun:test';
+import { type Schema, compileQuery } from '@/src/index';
+import type { Query } from '@/src/types/query';
+
+test('inline statement values', () => {
+  const query: Query = {
+    get: {
+      account: {
+        with: { handle: 'elaine' },
+      },
+    },
+  };
+
+  const schemas: Array<Schema> = [
+    {
+      slug: 'account',
+      fields: [
+        {
+          slug: 'handle',
+          type: 'string',
+        },
+      ],
+    },
+  ];
+
+  const { readStatement, values } = compileQuery(query, schemas, {
+    inlineValues: true,
+  });
+
+  expect(readStatement).toBe(
+    'SELECT * FROM "accounts" WHERE ("handle" = "elaine") LIMIT 1',
+  );
+  expect(values).toMatchObject([]);
+});
