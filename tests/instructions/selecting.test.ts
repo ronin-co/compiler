03bf4924813706e39e5e500e2f3d99eddf9b65ba
Leo Lamprecht <leo@ronin.co>
2024-12-02 14:58:42 +0100

    Added one more test case

--- tests/instructions/selecting.test.ts
+++ tests/instructions/selecting.test.ts
@@ -39,11 +39,11 @@ test('get single record with specific field', async () => {
   });
 });
 
-test('get single record with specific fields', () => {
+test('get single record with specific fields', async () => {
   const queries: Array<Query> = [
     {
       get: {
-        beach: {
+        product: {
           selecting: ['id', 'name'],
         },
       },
@@ -52,7 +52,7 @@ test('get single record with specific fields', () => {
 
   const models: Array<Model> = [
     {
-      slug: 'beach',
+      slug: 'product',
       fields: [
         {
           slug: 'name',
@@ -66,9 +66,17 @@ test('get single record with specific fields', () => {
 
   expect(transaction.statements).toEqual([
     {
-      statement: 'SELECT "id", "name" FROM "beaches" LIMIT 1',
+      statement: 'SELECT "id", "name" FROM "products" LIMIT 1',
       params: [],
       returning: true,
     },
   ]);
+
+  const rows = await queryEphemeralDatabase(models, transaction.statements);
+  const result = transaction.prepareResults(rows)[0] as SingleRecordResult;
+
+  expect(result.record).toMatchObject({
+    id: expect.stringMatching(RECORD_ID_REGEX),
+    name: expect.any(String),
+  });
 });
