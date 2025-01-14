dc528490749cde98f646a650e1f3a059b8c74461
Leo Lamprecht <leo@ronin.co>
2025-01-14 19:42:18 +0100

    Ensure faster code

--- src/instructions/selecting.ts
+++ src/instructions/selecting.ts
@@ -105,6 +105,20 @@ export const handleSelecting = (
               return !(field.type === 'link' && field.kind === 'many');
             });
 
+        // If the column names should be expanded, that means we need to alias all
+        // columns of the joined table to avoid conflicts with the root table.
+        if (expandColumns) {
+          for (const field of queryModelFields) {
+            const newValue = parseFieldExpression(
+              { ...subQueryModel, tableAlias },
+              'including',
+              `${QUERY_SYMBOLS.FIELD}${field.slug}`,
+            );
+
+            instructions.including![`${tableAlias}.${field.slug}`] = newValue;
+          }
+        }
+
         const { loadedFields: nestedLoadedFields } = handleSelecting(
           models,
           { ...subQueryModel, tableAlias },
@@ -118,20 +132,6 @@ export const handleSelecting = (
         );
 
         loadedFields.push(...nestedLoadedFields);
-
-        for (const field of queryModelFields) {
-          // If the column names should be expanded, that means we need to alias all
-          // columns of the joined table to avoid conflicts with the root table.
-          if (expandColumns) {
-            const newValue = parseFieldExpression(
-              { ...subQueryModel, tableAlias },
-              'including',
-              `${QUERY_SYMBOLS.FIELD}${field.slug}`,
-            );
-
-            instructions.including![`${tableAlias}.${field.slug}`] = newValue;
-          }
-        }
 
         continue;
       }
