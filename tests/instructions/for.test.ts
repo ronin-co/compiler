7d81a47ec7df0ff6f9c6eb109aadbb3382c2e4e9
Leo Lamprecht <leo@ronin.co>
2025-01-17 19:11:54 +0100

    Ensure perfect results for one-to-many relationships

--- tests/instructions/for.test.ts
+++ tests/instructions/for.test.ts
@@ -525,7 +525,7 @@ test('get single record including child records (one-to-many, defined manually)'
 
   expect(transaction.statements).toEqual([
     {
-      statement: `SELECT * FROM (SELECT * FROM "beaches" LIMIT 1) as sub_beaches LEFT JOIN "ronin_link_beach_visitors" as including_visitors ON ("including_visitors"."source" = "sub_beaches"."id") LEFT JOIN "accounts" as including_ronin_root ON ("including_ronin_root"."id" = "including_visitors"."target")`,
+      statement: `SELECT "sub_beaches"."id", "sub_beaches"."ronin.locked", "sub_beaches"."ronin.createdAt", "sub_beaches"."ronin.createdBy", "sub_beaches"."ronin.updatedAt", "sub_beaches"."ronin.updatedBy", "including_visitors"."id" as "visitors[0].id", "including_visitors"."ronin.locked" as "visitors[0].ronin.locked", "including_visitors"."ronin.createdAt" as "visitors[0].ronin.createdAt", "including_visitors"."ronin.createdBy" as "visitors[0].ronin.createdBy", "including_visitors"."ronin.updatedAt" as "visitors[0].ronin.updatedAt", "including_visitors"."ronin.updatedBy" as "visitors[0].ronin.updatedBy", "including_ronin_root"."id" as "visitors[0].id", "including_ronin_root"."ronin.locked" as "visitors[0].ronin.locked", "including_ronin_root"."ronin.createdAt" as "visitors[0].ronin.createdAt", "including_ronin_root"."ronin.createdBy" as "visitors[0].ronin.createdBy", "including_ronin_root"."ronin.updatedAt" as "visitors[0].ronin.updatedAt", "including_ronin_root"."ronin.updatedBy" as "visitors[0].ronin.updatedBy", "including_ronin_root"."handle" as "visitors[0].handle", "including_ronin_root"."firstName" as "visitors[0].firstName" FROM (SELECT * FROM "beaches" LIMIT 1) as sub_beaches LEFT JOIN "ronin_link_beach_visitors" as including_visitors ON ("including_visitors"."source" = "sub_beaches"."id") LEFT JOIN "accounts" as including_ronin_root ON ("including_ronin_root"."id" = "including_visitors"."target")`,
       params: [],
       returning: true,
     },
@@ -545,8 +545,6 @@ test('get single record including child records (one-to-many, defined manually)'
     },
     visitors: new Array(2).fill({
       id: expect.stringMatching(RECORD_ID_REGEX),
-      source: expect.stringMatching(RECORD_ID_REGEX),
-      target: expect.stringMatching(RECORD_ID_REGEX),
       ronin: {
         locked: false,
         createdAt: expect.stringMatching(RECORD_TIMESTAMP_REGEX),
